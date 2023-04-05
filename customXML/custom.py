4c6295d71f3b72ac0415e2ad12353039fc2653df
Keerthana S <105709569+keerthu1205@users.noreply.github.com>
2023-04-05 19:28:03 +0530

    Create custom.py

---
+++ customXML/custom.py
@@ -0,0 +1,42 @@
+from ansible.module_utils.basic import AnsibleModule
+import xml.etree.ElementTree as ET
+
+def run_module():
+    module_args = dict(
+        xml_file=dict(type='str', required=True),
+    )
+    result = dict(
+        changed=False,
+        original_message='',
+        message=''
+    )
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+
+    xml_file = module.params['xml_file']
+
+    if module.check_mode:
+        module.exit_json(**result)
+
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+
+    food_price_list = []
+
+    for item in root.iter('item'):
+        food_name = item.find('name').text
+        price = item.find('price').text
+        food_price_list.append({"name": food_name, "price": price})
+
+    result['food_price_list'] = food_price_list
+    result['changed'] = True
+
+    module.exit_json(**result)
+
+def main():
+    run_module()
+
+if __name__ == '__main__':
+    main()
